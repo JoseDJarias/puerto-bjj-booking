9eacc33b9adf9cdaedbdb1ee3cdd2045985e2e87
Owen <135662974+OwenEscobar0@users.noreply.github.com>
2026-02-10 08:44:00 -0600

    add admin routes for the new functionalities
    -Admin module > MembershipPlans
    -Admin module > ClassTypes
    -Admin module > Users
    -Admin module > Membership

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,19 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
 
+  namespace :admin do
+    root "dashboard#index"
+
+    resources :membership_plans, path: "planes"
+    resources :class_types
+    resources :membership_packages, path: "paquetes"
+    resources :users do
+      member do
+        patch :approve #Generates: /admin/users/:id/approve
+      end
+    end
+    resources :memberships, path: "membresias"
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 end

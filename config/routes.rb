66370fb656e656fb64ca6134ec3b4c78e25dfeea
Owen <135662974+OwenEscobar0@users.noreply.github.com>
2026-02-11 00:38:36 -0600

    add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
 
+  resources :class_schedules, only: [:index] 
+  resources :bookings, only: [:create]
+
   namespace :admin do
     root "dashboard#index"
 
@@ -17,6 +20,19 @@ Rails.application.routes.draw do
       end
     end
     resources :memberships, path: "membresias"
+    resources :class_schedules do
+      collection do
+        # Routes for the bulk generator
+        get :batch_new    # The "Generate Schedule" form
+        post :batch_create # The action that creates the records
+      end
+    end
+      # Controller dedicated to the management of reservations by the admin
+    resources :bookings, only: [:create, :update, :destroy] do
+      member do
+        patch :check_in # Route to mark attendance quickly
+      end
+    end
   end
 
   get "up" => "rails/health#show", as: :rails_health_check

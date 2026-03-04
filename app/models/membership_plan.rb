cf49a8108be14d21de6ee695290800f1433748de
JoseDJarias <arias_da044@hotmail.com>
2026-03-04 15:56:51 -0600

    add membership_pricings to membership_plan

--- app/models/membership_plan.rb
+++ app/models/membership_plan.rb
@@ -1,20 +1,14 @@
 class MembershipPlan < ApplicationRecord
   has_many :memberships
+  has_many :membership_pricings, dependent: :destroy
 
   validates :name, :duration_months, :price, presence: true
   validates :duration_months, numericality: { greater_than_or_equal_to: 0 } # Permitir 0 para drop-in
-  validates :price, numericality: { greater_than_or_equal_to: 0 }
 
   scope :active, -> { where(active: true) }
-  scope :drop_in, -> { where(duration_months: 0) }
-  scope :recurring, -> { where("duration_months > 0") }
 
   def display_name
-    return "#{name} (1 día)" if duration_months == 0
     "#{name} (#{duration_months} #{'mes'.pluralize(duration_months)})"
   end
-
-  def drop_in?
-    duration_months == 0
-  end
+  
 end

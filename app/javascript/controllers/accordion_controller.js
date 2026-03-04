8059c65d3d8e3512efa0481b9958f0a9cb3ae66d
JoseDJarias <arias_da044@hotmail.com>
2026-03-04 15:56:16 -0600

    add accordion stimulus

---
+++ app/javascript/controllers/accordion_controller.js
@@ -0,0 +1,27 @@
+// app/javascript/controllers/accordion_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["wrapper", "icon", "card"]
+
+  connect() {
+  }
+
+  toggle(event) {
+    if (window.innerWidth >= 1024) return
+    event.preventDefault()
+
+    this.wrapperTarget.classList.toggle("grid-rows-[0fr]")
+    this.wrapperTarget.classList.toggle("grid-rows-[1fr]")
+    
+    this.wrapperTarget.classList.toggle("opacity-0")
+    this.wrapperTarget.classList.toggle("opacity-100")
+
+    this.iconTarget.classList.toggle("rotate-180")
+
+    this.cardTarget.classList.toggle("ring-indigo-500")
+    this.cardTarget.classList.toggle("ring-2")
+    this.cardTarget.classList.toggle("ring-slate-200")
+    this.cardTarget.classList.toggle("shadow-md")
+  }
+}

3413068224e6458b12515cb23a1ae5fb526147f4
JoseDJarias <arias_da044@hotmail.com>
2026-03-03 23:46:26 -0600

    add drop_in_tickets relation at user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
   has_many :memberships, dependent: :destroy
+  has_many :drop_in_tickets, dependent: :destroy
 
   validates :email_address, presence: true, uniqueness: true
   normalizes :email_address, with: ->(e) { e.strip.downcase }

5764a2cea1c1ba932d3f48a382fbbd1f7a1caf65
JoseDJarias <arias_da044@hotmail.com>
2026-03-06 16:14:36 -0600

    add identification and nickname validation and simple methods to display name and legal name

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,14 @@ class User < ApplicationRecord
 
   validates :email_address, presence: true, uniqueness: true
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+  validates :identification, presence: true, uniqueness: true, on: :update # Opcional: obligatorio solo al completar perfil
+  validates :nickname, uniqueness: true, allow_blank: true
+
+  def display_name
+    nickname.presence || first_name
+  end
+  
+  def full_legal_name
+    "#{first_name} #{last_name}"
+  end
 end

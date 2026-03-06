ada1ae052b24a318bac4dceff8c48cefd78f7b75
JoseDJarias <arias_da044@hotmail.com>
2026-03-06 16:05:55 -0600

    Add Identification and Nickname to users table

---
+++ db/migrate/20260306220515_add_identification_and_nickname_to_users.rb
@@ -0,0 +1,6 @@
+class AddIdentificationAndNicknameToUsers < ActiveRecord::Migration[8.1]
+  def change
+    add_column :users, :identification, :string
+    add_column :users, :nickname, :string
+  end
+end

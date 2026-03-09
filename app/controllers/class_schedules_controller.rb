1ec4463552083bfd4fe0952d74aaa64847ad9456
JoseDJarias <arias_da044@hotmail.com>
2026-03-09 15:15:31 -0600

    feat: optimize class schedules controller for weekly view and lazy participants loading and add show method to see a class details

--- app/controllers/class_schedules_controller.rb
+++ app/controllers/class_schedules_controller.rb
@@ -9,17 +9,28 @@ class ClassSchedulesController < ApplicationController
                                     .includes(:class_type, :instructor, :bookings)
   end
 
+  def show
+    @schedule = ClassSchedule.find(params[:id])
+    @booking = current_user.bookings.find_by(class_schedule: @schedule)
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
+  end
+
   def week
+    @user = Current.user # Importante para las reglas de acceso
     @week_start = week_param.presence&.to_date || Date.current.beginning_of_week(:monday)
     @week_start = @week_start.beginning_of_week(:monday)
     @week_end = @week_start + 6.days
     @days = (@week_start..@week_end).to_a
-
-    schedules = ClassSchedule.active
-                             .for_range(@week_start, @week_end)
-                             .includes(:class_type, :instructor)
-                             .order(:starts_at)
-    @schedules_by_date = schedules.group_by(&:date)
+  
+    @schedules_by_date = ClassSchedule.active
+                                      .for_range(@week_start, @week_end)
+                                      .includes(:class_type, :instructor, :bookings)
+                                      .order(:starts_at)
+                                      .group_by(&:date)
   end
 
   def participants
@@ -28,11 +39,11 @@ class ClassSchedulesController < ApplicationController
     respond_to do |format|
       format.html do
         if turbo_frame_request?
-          render partial: "class_schedules/participants_list", 
+          render partial: "class_schedules/partials/participants_list", 
                  locals: { schedule: @schedule },
                  layout: false
         else
-          render partial: "class_schedules/participants_list", locals: { schedule: @schedule }
+          render partial: "class_schedules/partials/participants_list", locals: { schedule: @schedule }
         end
       end
     end

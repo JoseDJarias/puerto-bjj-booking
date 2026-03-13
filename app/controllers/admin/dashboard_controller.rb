11695c69c65f3e804241933da733d4dc8c0305fe
JoseDJarias <arias_da044@hotmail.com>
2026-03-13 03:02:53 -0600

    Let instructor see the DashboardController, get inheretance from OperationsController, modify index methods, change to be a useful index dahsboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,22 @@
+# app/controllers/admin/dashboard_controller.rb
 module Admin
-  class DashboardController < BaseController
+  class DashboardController < OperationsController
     def index
+      @base_date = ClassSchedule.logical_today 
+  
+      @offset = params[:offset].to_i
+      
+      @date = @base_date + @offset.days
+      
+      # Get the classes for that specific operative date
+      scope = current_user.admin? ? ClassSchedule : ClassSchedule.where(instructor: current_user)
+      @schedules = scope.for_date(@date)
+                        .includes(:class_type, :instructor, :bookings) # Cargamos todos los bookings de una vez
+                        .order(:starts_at)
+    
+      if current_user.admin?
+        @pending_approvals = User.where(approved_at: nil).count
+      end
     end
-
-  end  
+  end
 end

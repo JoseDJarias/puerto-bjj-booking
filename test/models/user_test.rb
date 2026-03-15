8a46001797848faab4255726f71b0a9f62947e08
JoseDJarias <arias_da044@hotmail.com>
2026-03-14 21:41:08 -0600

    add test to validate first and last name

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,26 +5,41 @@ class UserTest < ActiveSupport::TestCase
     @user = users(:admin) # Asumiendo que tienes fixtures
   end
 
+  # --- Tests de Nombre y Apellido ---
+ 
+  test "no debe ser válido sin nombre" do
+    user = User.new(last_name: "Test", identification: "112340567", email_address: "no.nombre@test.com", password: "password123")
+    user.valid?
+    
+    assert_includes user.errors.full_messages, "Nombre no puede estar en blanco"
+  end
+  
+  test "no debe ser válido sin apellido" do
+    user = User.new(first_name: "Daniel", identification: "112340567", email_address: "no.apellido@test.com", password: "password123")
+    user.valid?
+    
+    assert_includes user.errors.full_messages, "Apellido no puede estar en blanco"
+  end
   # --- Tests de Identificación ---
 
   test "debe normalizar la identificación (quitar guiones y subir a mayúsculas)" do
-    user = User.new(identification: "1-1234-5678", email_address: "nuevo@test.com", password: "password123")
+    user = User.new(first_name: "Daniel", last_name: "Test" ,identification: "1-1234-5678", email_address: "nuevo@test.com", password: "password123")
     user.validate
     assert_equal "112345678", user.identification
   end
 
   test "debe ser válido con cédula de Costa Rica (9 dígitos)" do
-    user = User.new(identification: "112340567", email_address: "tico@test.com", password: "password123")
+    user = User.new(first_name: "Daniel", last_name: "Test" ,identification: "112340567", email_address: "tico@test.com", password: "password123")
     assert user.valid?, "Debería ser válida una cédula de 9 dígitos"
   end
 
   test "debe ser válido con pasaporte extranjero (alfanumérico)" do
-    user = User.new(identification: "PAS123456", email_address: "ext@test.com", password: "password123")
+    user = User.new(first_name: "Daniel", last_name: "Test" ,identification: "PAS123456", email_address: "ext@test.com", password: "password123")
     assert user.valid?, "Debería ser válido un pasaporte alfanumérico"
   end
 
   test "debe fallar si la identificación no cumple ningún formato" do
-    user = User.new(identification: "123") # Muy corto
+    user = User.new(first_name: "Daniel", last_name: "Test" ,identification: "123") # Muy corto
     assert_not user.valid?
     assert_includes user.errors[:identification], "debe ser una cédula de 9 dígitos o un pasaporte válido (6-15 caracteres)"
   end
@@ -32,7 +47,7 @@ class UserTest < ActiveSupport::TestCase
   # --- Tests de Contraseña ---
 
   test "debe requerir contraseña de mínimo 8 caracteres al crear" do
-    user = User.new(password: "1234567")
+    user = User.new(first_name: "Daniel", last_name: "Test" ,password: "1234567")
     assert_not user.valid?
     assert_includes user.errors[:password], "es demasiado corto (mínimo 8 caracteres)"
   end
@@ -47,6 +62,8 @@ class UserTest < ActiveSupport::TestCase
 
   test "debe validar la longitud si se intenta cambiar la contraseña" do
     user = users(:admin)
+    user.first_name = "Daniel"
+    user.last_name = "Test"
     user.password = "123"
     
     assert_not user.save, "No debería permitir cambiar a una contraseña corta"

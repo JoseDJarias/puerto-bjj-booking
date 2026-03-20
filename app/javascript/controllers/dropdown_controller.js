8d360556d83ec00e7bdf669d5c72a732fa19641e
JoseDJarias <arias_da044@hotmail.com>
2026-03-20 09:27:07 -0600

    remove select modality dropdown inline js logic to dropdown controller stimulus

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "menu" ]
+  static targets = [ "menu", "input", "label" ]
 
   toggle() {
     this.menuTarget.classList.toggle("hidden")
@@ -11,5 +11,19 @@ export default class extends Controller {
     if (!this.element.contains(event.target)) {
       this.menuTarget.classList.add("hidden")
     }
+  }
+
+  // Se activa al hacer clic en una opción (Gi, No Gi, etc.)
+  select(event) {
+    const { value, label } = event.currentTarget.dataset
+    
+    // 1. Cambia el valor del input hidden para Rails
+    if (this.hasInputTarget) this.inputTarget.value = value
+    
+    // 2. Cambia el texto visible en el botón
+    if (this.hasLabelTarget) this.labelTarget.innerText = label
+    
+    // 3. Cierra el menú
+    this.menuTarget.classList.add("hidden")
   }
 }

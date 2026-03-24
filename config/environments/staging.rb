828cf4d239c053f63ebb03d213e085a85f03d347
JoseDJarias <arias_da044@hotmail.com>
2026-03-23 23:00:12 -0600

    enable staging enviroment to handle resend

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -59,10 +59,10 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "https://staging.booking.puertojiujitsu.com" }
+  config.action_mailer.default_url_options = { host: "staging.booking.puertojiujitsu.com" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
@@ -72,6 +72,9 @@ Rails.application.configure do
   #   port: 587,
   #   authentication: :plain
   # }
+
+  config.action_mailer.delivery_method = :resend
+  config.action_mailer.resend_settings = { api_key: ENV["RESEND_API_KEY"] }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).

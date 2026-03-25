b1bd16a84e395c9d259d7f7052f4d7d1a4eee229
JoseDJarias <arias_da044@hotmail.com>
2026-03-24 21:23:58 -0600

    config prod to use resend as a Transactional Email Service

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,10 +55,10 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  config.action_mailer.default_url_options = { host: "booking.puertojiujitsu.com" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   # config.action_mailer.smtp_settings = {
@@ -68,6 +68,9 @@ Rails.application.configure do
   #   port: 587,
   #   authentication: :plain
   # }
+
+  config.action_mailer.delivery_method = :resend
+  config.action_mailer.resend_settings = { api_key: ENV["RESEND_API_KEY"] }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
